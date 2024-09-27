c5a356fc9579845c2088670f1bb3a6325f80f3a4
unknown <nguyenphuockhai1234123@gmail.com>
2024-09-27 03:51:55 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		assert force > 0;
@@ -33,23 +35,63 @@ public class Romain {
 		if (force > 0) {
 			parler("Aie");
 		} else {
-			parler("J'abandonne...");	
+			parler("J'abandonne...");
 		}
 		assert force < oldForce;
 	}
 
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2: {
+			System.out.println("le soldat " + nom + " est deja bien protege! ");
+			break;
+		}
+		case 1: {
+			if (equipements[0] == equipement) {
+				System.out.println("le soldat " + nom + " possede deja un " + equipement + "!");
+				break;
+			}
+
+			else {
+				equipement(equipement);
+				break;
+			}
+		}
+
+		default: {
+			equipement(equipement);
+			break;
+
+		}
+
+		}
+	}
+
+	private void equipement(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat " + nom + " s'equipe avec un " + equipement + ".");
+
+	}
+
 	public static void main(String[] args) {
-	
+
 //			System.out.println("Test avec force nulle ou négative");
 //			Romain minus = new Romain("Minus", -6);
-			Romain minus = new Romain("Minus", 6);
-			System.out.println("Test methode prendreParole");
-			minus.prendreParole();
-			System.out.println("Test methode parler");
-			minus.parler("Bonjour");
-			System.out.println("Test methode recevoirCoup");
-			minus.recevoirCoup(6);
+		Romain minus = new Romain("Minus", 6);
+		System.out.println("Test methode prendreParole");
+		minus.prendreParole();
+		System.out.println("Test methode parler");
+		minus.parler("Bonjour");
+		System.out.println("Test methode recevoirCoup");
+		minus.recevoirCoup(6);
 
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 
 	}
 }
